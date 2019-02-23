17a7563ec117c989c5d80b0fedbdb1fc843cd38c
Maria Kim <mariask98@gmail.com>
2019-02-23 16:19:56 -0500

    added in function names

---
+++ liftknockoff/liftjavascript.js
@@ -0,0 +1,27 @@
+var Lat = 0; 
+var Long = 0;
+var me = new google.maps.LatLng(myLat, myLng);
+
+function init() {
+
+}
+
+function get_location() {
+
+}
+
+function renderMap() {
+
+}
+
+function get_passengers() {
+
+}
+
+function get_driver() {
+
+}
+
+function weiner() {
+
+}
